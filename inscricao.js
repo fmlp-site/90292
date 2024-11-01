90d5351357558ebb76aa91fe469a7b41c9c04228
fmlp-site <133589405+fmlp-site@users.noreply.github.com>
2024-11-01 13:42:42 -0400

    Update inscricao.js

--- inscricao.js
+++ inscricao.js
@@ -6,11 +6,11 @@ document.getElementById("form-inscricao").addEventListener("submit", function(ev
     const curso = document.getElementById("curso").value;
     const idInscricao = "ID" + Math.floor(Math.random() * 100000);
 
-    fetch("https://<SEU-NOME-DO-BANCO>.restdb.io/rest/candidatos", {
+    fetch("https://vestibularb-c6f2.restdb.io/rest/candidatos?max=2", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "x-apikey": "<SUA-API-KEY>"
+            "x-apikey": "9c549e27cfe91b4251c8cfc0fea1fa058a163"
         },
         body: JSON.stringify({ nome, cpf, curso, idInscricao, status: "Aguardando" })
     })
