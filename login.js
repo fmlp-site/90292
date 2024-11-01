c27f911c0ff7caef30d9b92e37eb92ece5c46953
fmlp-site <133589405+fmlp-site@users.noreply.github.com>
2024-11-01 13:56:54 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,22 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
+document.getElementById("form-login").addEventListener("submit", function(event) {
     event.preventDefault();
-    
-    const idInscricao = document.getElementById("id_inscricao").value;
+
+    const idInscricao = document.getElementById("idInscricao").value;
 
     fetch(`https://vestibularb-c6f2.restdb.io/rest/candidatos?max=2?q={"idInscricao": "${idInscricao}"}`, {
         headers: {
+            "Content-Type": "application/json",
             "x-apikey": "9c549e27cfe91b4251c8cfc0fea1fa058a163"
         }
     })
     .then(response => response.json())
     .then(data => {
         if (data.length > 0) {
-            window.location.href = "paineldocandidato.html";
+            alert("Login realizado com sucesso!");
+            localStorage.setItem("candidatoId", data[0]._id); // Salva o ID do candidato para uso futuro
+            window.location.href = "paineldocandidato.html"; // Redireciona para o painel do candidato
         } else {
-            alert("ID de inscrição inválido.");
+            alert("ID de inscrição inválido. Tente novamente.");
         }
     })
     .catch(error => console.error("Erro:", error));
